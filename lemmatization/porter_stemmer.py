cbd6e7a7b67d42346e3fff5668cddfe4f009ab9a
petr-shumilov <pilot-shumilov@yandex.ru>
2019-01-11 16:03:42 +0300

    Fix stemmer

--- lemmatization/porter_stemmer.py
+++ lemmatization/porter_stemmer.py
@@ -30,7 +30,7 @@ def main():
 
     logger.info("Started")
     normalizer = Normalizer()
-    stemmas = defaultdict(set)
+    #stemas = defaultdict(set)
 
     frequency = defaultdict(lambda: 0)
     count = 0
@@ -41,7 +41,7 @@ def main():
             if count % 1000 == 0:
                 logger.info("%d samples have been handled" % (count))
             if count % 20000 == 0:
-                logger.info(json.dumps(stemmas, 
+                logger.info(json.dumps(stemas, 
                                         default=lambda x: list(x) if isinstance(x, set) else x.__dict__, 
                                         ensure_ascii=False))
 
@@ -56,23 +56,24 @@ def main():
             .fix_spelling(corrector)
 
             for word in preprocessed_text.split():
-                stemma = normalizer.stem(word)
-                stemmas[stemma].add(word)
-                frequency[stemma] += 1
+                stema = normalizer.stem(word)
+                stemas[stema].add(word)
+                frequency[stema] += 1
 
             count += 1
-
         except Exception as e:
             logger.error("Error: %s\nData: %s" % (str(e),(line[3] if len(line) >= 4 else line )))
             count += 1            
             
 
 
-    logger.info("Done!")
-    logger.info(json.dumps(stemmas, 
+    logger.info(json.dumps(stemas, 
                             default=lambda x: list(x) if isinstance(x, set) else x.__dict__, 
                             ensure_ascii=False))
     logger.info(json.dumps(frequency, ensure_ascii=False))
+    logger.info("Done!")
+    logger.info("Amount of unique stemas: {}".format(len(frequency)))
+    
 
 if __name__ == '__main__':
     main()
